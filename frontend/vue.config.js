3eb0bceaabd93270afd86d379e16193f14488232
gabrielajohnson <gabrielagj23@gmail.com>
2022-11-25 22:25:51 -0500

    trying to update production

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -9,8 +9,8 @@ module.exports = defineConfig({
         },
       },
     },
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/production-sub-path/'
-    : '/'
   },
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/production-sub-path/'
+  : '/'
 })
